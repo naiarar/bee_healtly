90880b7cd70e24a71663360a9b4f2461a1f2b8e9
naiarar <rnaiara92@gmail.com>
2023-08-31 16:04:55 -0300

    urls

--- backend/beehealtly/urls.py
+++ backend/beehealtly/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 
 from rest_framework import routers
+from users.api import viewsets as usersviewsets
 
 route = routers.DefaultRouter()
-
+route.register(r'users', usersviewsets.UsersViewset, basename="Users")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', include(route.urls))
 ]
